825ab8afbc030a90281bf21656da6c123efda2db
cvanpoelje <c.vanpoelje@developers.nl>
2019-07-09 17:21:30 +0200

    Using the Fetch api get mockdata from jsonplaceholder.

--- multiplayerGame/views/Rankings.js
+++ multiplayerGame/views/Rankings.js
@@ -1,8 +1,32 @@
+// --------------------------------
+//  Define Data Sources
+// --------------------------------
+
+let getUsersList = async () => {
+    let url="https://jsonplaceholder.typicode.com/users";
+    const options = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+    try {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/users`, options);
+        const json = await response.json();
+        console.log(json);
+        return json
+    } catch (err) {
+        console.log('Error getting documents', err)
+    }
+};
+
+
 let Rankings = {
     render: async () => {
+        getUsersList();
         let view =  /*html*/`
             <h1>Rankings!</h1>
-`
+`;
         return view
     }
     , after_render: async () => {
